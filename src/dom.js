f5bfce0b68ae87fb99a2f3b02aaffb82548bbfb0
rx <132195314+victorxph@users.noreply.github.com>
2023-12-21 13:14:04 -0300

    Add initial DOM management and fix grid layout bug

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,44 @@
-import './index.js'
+import Project from './project.js';
 
 export default class domHandler {
 
-	  
-	
+	projectDiv = document.querySelector('.projects');
+	projectList = document.querySelector('.project-list');
+
+	addProjectBtn = document.querySelector('.add-project');
+
+	main = document.querySelector('main')
+
+	setListeners() {
+		this.addProjectBtn.addEventListener('click',)
+	}
+
+	addProject(name) {
+		const project = new Project(name);
+		const projectCard = document.createElement('div');
+		projectCard.classList.add('project-card');
+		projectCard.innerHTML = `<h2 class="project-name">${project.name}</h2>
+					<ul class="card-task-list">
+						<li>
+							<input type="checkbox" name="task-check" id="task-check">
+							<span class="task-content">Lorem ipsum dolor sit amet consectetur adipisicing elit..</span>
+						</li>
+						<li>
+							<input type="checkbox" name="task-check" id="task-check">
+							<span class="task-content">Lorem ipsum dolor sit amet consectetur adipisicing elit..</span>
+						</li>
+						<li>
+							<input type="checkbox" name="task-check" id="task-check">
+							<span class="task-content">Lorem ipsum dolor sit amet consectetur adipisicing elit..</span>
+						</li>
+						<li>
+							<input type="checkbox" name="task-check" id="task-check">
+							<span class="task-content">Lorem ipsum dolor sit amet consectetur adipisicing elit..</span>
+						</li>
+					</ul>
+			<button type="button"><img width="30" height="30"
+					src="https://img.icons8.com/material-rounded/512/d4d4d4/plus-math--v1.png"
+					alt="plus-math--v1" /></button>`
+		this.main.appendChild(projectCard);
+	}
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import './style.scss'
 import Task from './task.js'
 import Project from './project.js'
 import ProjectHandler from './handler.js'
+import domHandler from './dom.js'
 
 const handler = new ProjectHandler();
 const todo = handler.createProject('To do');
 
 const proj = handler.createProject('Foo');
 const whtvr = handler.createTask('Doo', 'Do whatever', false, '14/12/2023', 2, 'fdsfdsfds');
+
+const handlerDOM = new domHandler();
+handlerDOM.addProject('Foo');
+handlerDOM.addProject('Baa');
