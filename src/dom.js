b909212738ffdf8e9be005b3e35600bac74a477a
rx <132195314+victorxph@users.noreply.github.com>
2023-12-23 12:19:54 -0300

    Fix project submiting with enter and add capitalize function for project title

--- src/dom.js
+++ src/dom.js
@@ -7,23 +7,40 @@ export default class domHandler {
 
 	addProjectBtn = document.querySelector('.add-project');
 
-	main = document.querySelector('main')
+	main = document.querySelector('main');
 
 	projectModal = document.querySelector('.project-modal');
 	closePjModal = document.querySelector('.close-dialog');
 	submitProjectBtn = document.querySelector('.add-pj-btn')
 	projectNameInput = document.querySelector('.pj-name-input');
 
-	setListeners(callCase) {
+	capitalize(str) {
+		if (typeof str !== 'string' || str.length == 0) {
+			return str
+		}
+
+		return str.charAt(0).toUpperCase() + str.slice(1)
+	}
+
+	setListeners() {
 		this.addProjectBtn.addEventListener('click', this.openProjectModal.bind(this))
 		this.closePjModal.addEventListener('click', this.closeProjectModal.bind(this))
-
-		if (callCase == 'openModal') {
-			this.submitProjectBtn.addEventListener('click', function() {
-				console.log(this.projectNameInput)
+		this.projectModal.addEventListener('keydown', function(e) {
+			if (e.key == 'Escape' || e.keyCode == 27) {
+				this.closeProjectModal();
+			} else if (!this.projectNameInput.value && (e.key == 'Enter' || e.keyCode == 13)) {
+				e.preventDefault();
+				this.closeProjectModal();
+			} else if (this.projectNameInput.value && (e.key == 'Enter' || e.keyCode == 13)) {
+				e.preventDefault();
 				this.addProject(this.projectNameInput.value)
-			}.bind(this))
-		}
+			}
+		}.bind(this))
+
+		this.submitProjectBtn.addEventListener('click', function() {
+			console.log(this.projectNameInput)
+			this.addProject(this.projectNameInput.value)
+		}.bind(this))
 	}
 
 	openProjectModal() {
@@ -32,22 +49,6 @@ export default class domHandler {
 		this.projectModal.style.justifyContent = 'space-between';
 		this.projectModal.showModal();
 		this.closePjModal = document.querySelector('.close-dialog');
-		this.projectModal.addEventListener('keydown', function(e) {
-			if (e.key == 'Escape' || e.keyCode == 27) {
-				this.projectModal.style.display = '';
-				this.projectModal.style.flexDirection = '';
-				this.projectModal.style.justifyContent = '';
-
-				this.projectModal.close()
-			} else if (e.key == 'Enter' && this.projectNameInput == '') {
-				this.projectModal.style.display = '';
-				this.projectModal.style.flexDirection = '';
-				this.projectModal.style.justifyContent = '';
-
-				this.projectModal.close()
-			}
-		}.bind(this))
-		this.setListeners('openModal')
 	}
 
 	closeProjectModal() {
@@ -61,7 +62,7 @@ export default class domHandler {
 		const project = new Project(name);
 		const projectCard = document.createElement('div');
 		projectCard.classList.add('project-card');
-		projectCard.innerHTML = `<h2 class="project-name">${project.name}</h2>
+		projectCard.innerHTML = `<h2 class="project-name">${this.capitalize(project.name)}</h2>
 					<ul class="card-task-list">
 						<li>
 							<input type="checkbox" name="task-check" id="task-check">
@@ -84,5 +85,7 @@ export default class domHandler {
 					src="https://img.icons8.com/material-rounded/512/d4d4d4/plus-math--v1.png"
 					alt="plus-math--v1" /></button>`
 		this.main.appendChild(projectCard);
+		this.projectNameInput.value = '';
+		this.closeProjectModal();
 	}
 }
